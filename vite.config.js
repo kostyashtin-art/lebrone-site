0c83627e2d4f15a8bed5332ff511d9a1020763e3
kostyashtin-art <kostyashtin@gmail.com>
2026-09-24 17:34:54 +0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,2 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "/lebrone-site/",
-  plugins: [react()]
-});
+import { defineConfig } from "vite"; import react from "@vitejs/plugin-react";
+export default defineConfig({base:"/lebrone-site/",plugins:[react()]});
